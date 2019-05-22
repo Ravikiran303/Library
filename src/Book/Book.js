2efcdae226c7cf49b593716bd4a5f39e71ed84df
Ravikiran303 <ravikiran2221@gmail.com>
2019-05-22 21:39:04 +0530

    Add Dummy Book

--- src/Book/Book.js
+++ src/Book/Book.js
@@ -2,7 +2,14 @@ import React, { Component } from "react";
 
 export class Book extends Component {
   render() {
-    return <div>Book</div>;
+    return (
+      <div>
+        <img src="https://images-na.ssl-images-amazon.com/images/I/91WJQeRImJL.jpg" />
+        <label>Title:</label>
+        <label>Author:</label>
+        <label>Description:</label>
+      </div>
+    );
   }
 }
 
