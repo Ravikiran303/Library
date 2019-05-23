90b30ab8a3a0919373800ae8d133a30f7583d175
Ravikiran303 <ravikiran2221@gmail.com>
2019-05-23 11:03:42 +0530

    Add multiple books with proper styling

--- src/Books/Books.js
+++ src/Books/Books.js
@@ -1,27 +1,28 @@
 import React, { Component } from "react";
 import Book from "../Book/Book";
+import "./style.css";
 
 export class Books extends Component {
   render() {
     return (
-      <div>
+      <div className="books">
         <Book
           image="https://images-na.ssl-images-amazon.com/images/I/91WJQeRImJL.jpg"
-          title="Comic"
+          title="Super Heros"
           author="Ravi"
           description="about SuperHeros"
         />
         <Book
-          image="https://images-na.ssl-images-amazon.com/images/I/91WJQeRImJL.jpg"
-          title="Comic"
+          image="https://pictures.abebooks.com/PRINTMATTERS/18446485333_5.jpg"
+          title="Black Cat"
           author="Ravi"
-          description="about SuperHeros"
+          description="........."
         />
         <Book
-          image="https://images-na.ssl-images-amazon.com/images/I/91WJQeRImJL.jpg"
-          title="Comic"
+          image="https://imgix.ranker.com/user_node_img/104/2075534/original/spider-man-photo-u76?w=650&q=50&fm=pjpg&fit=crop&crop=faces"
+          title="Spiderman"
           author="Ravi"
-          description="about SuperHeros"
+          description="........."
         />
       </div>
     );
