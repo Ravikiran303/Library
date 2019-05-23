9f8c6ad0f9ef2bceb774ce790c4588bbc69bcc03
Ravikiran303 <ravikiran2221@gmail.com>
2019-05-23 15:29:16 +0530

    Add array to store list of book insted of component

--- src/Books/Books.js
+++ src/Books/Books.js
@@ -2,34 +2,39 @@ import React, { Component } from "react";
 import Book from "../Book/Book";
 import "./style.css";
 
+const Books_list = [
+  {
+    image: "https://images-na.ssl-images-amazon.com/images/I/91WJQeRImJL.jpg",
+    title: "Super Heros",
+    author: "Ravi",
+    description: ".........."
+  },
+  {
+    image: "https://pictures.abebooks.com/PRINTMATTERS/18446485333_5.jpg",
+    title: "Black Cat",
+    author: "Ravi",
+    description: "........."
+  },
+  {
+    image:
+      "https://imgix.ranker.com/user_node_img/104/2075534/original/spider-man-photo-u76?w=650&q=50&fm=pjpg&fit=crop&crop=faces",
+    title: "Spiderman",
+    author: "Ravi",
+    description: ".........."
+  },
+  {
+    image:
+      "https://www.tanbooks.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/2/7/2754x_1.jpg",
+    title: "Acts",
+    author: "Ravi",
+    description: ".........."
+  }
+];
 export class Books extends Component {
   render() {
     return (
       <div className="books">
-        <Book
-          image="https://images-na.ssl-images-amazon.com/images/I/91WJQeRImJL.jpg"
-          title="Super Heros"
-          author="Ravi"
-          description=".........."
-        />
-        <Book
-          image="https://pictures.abebooks.com/PRINTMATTERS/18446485333_5.jpg"
-          title="Black Cat"
-          author="Ravi"
-          description="........."
-        />
-        <Book
-          image="https://imgix.ranker.com/user_node_img/104/2075534/original/spider-man-photo-u76?w=650&q=50&fm=pjpg&fit=crop&crop=faces"
-          title="Spiderman"
-          author="Ravi"
-          description="........."
-        />
-        <Book
-          image="https://www.tanbooks.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/2/7/2754x_1.jpg"
-          title="Acts"
-          author="Ravi"
-          description="........."
-        />
+        <Book />
       </div>
     );
   }
